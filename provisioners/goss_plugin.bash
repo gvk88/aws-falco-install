d457028bcdd6aff70f2a7aa680d2ed3c705ea374
gvk88 <vamsikrishna007.g@gmail.com>
2021-03-20 00:29:25 +0530

    Update goss_plugin.bash

--- provisioners/goss_plugin.bash
+++ provisioners/goss_plugin.bash
@@ -6,7 +6,7 @@ set -o errexit
 
 os_type="$(uname)"
 os_arch="amd64"
-version="v2.0.0"
+version="v3.0.0"
 echo "install packer-provisioner-goss-$version-$os_type-$os_arch.tar.gz"
 curl -L https://github.com/YaleUniversity/packer-provisioner-goss/releases/download/$version/packer-provisioner-goss-$version-$os_type-$os_arch.tar.gz -o packer-provisioner-goss.tar.gz
 tar -xzf packer-provisioner-goss.tar.gz
